bef6bc24e06d8653a638d80f4cbf8ec3c2020d0b
umaisshika <63295568+umaisshika@users.noreply.github.com>
2020-10-23 12:48:51 +0900

    [add]ユーザー登録ページマークアップ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    controllers: { 
+      registrations: 'users/registrations',
+      sessions: 'users/sessions' 
+    }
   root 'home#top'
 end
