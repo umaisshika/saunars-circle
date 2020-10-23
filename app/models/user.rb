d9019f65464a56ae712e7230738f5f93ca3fd361
umaisshika <63295568+umaisshika@users.noreply.github.com>
2020-10-23 14:33:59 +0900

    [add]ユーザー新規登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # emailを小文字に変換
+  before_save { self.email = email.downcase }
+  VALID_PASSWORD_REGEX =              /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,100}\z/i
+  validates :password, :password_confirmation,       allow_blank: true, format: { with: VALID_PASSWORD_REGEX }
+  validates :name,                                   presence: true, length: {maximum: 50}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users,
-    controllers: { 
-      registrations: 'users/registrations',
-      sessions: 'users/sessions' 
-    }
+  devise_for :users,controllers: { 
+    registrations: 'users/registrations',
+    sessions: 'users/sessions' 
+  }
   root 'home#top'
 end
