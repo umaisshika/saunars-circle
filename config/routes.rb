9d9cde8fbca7eaaa9902ce1b3735b8e7404142de
umaisshika <63295568+umaisshika@users.noreply.github.com>
2020-10-24 19:40:18 +0900

    [update]トップページ：サ飯のリンクを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'foods/index'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
   resources :users, only: [:show, :index]
+  resources :foods
   root 'home#top'
 end
