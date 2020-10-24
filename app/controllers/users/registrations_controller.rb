38177c48685bdda45ae6c0d3212a2f8e031c2d93
umaisshika <63295568+umaisshika@users.noreply.github.com>
2020-10-24 14:42:56 +0900

    [fix]rubocopによるコード修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -16,14 +16,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # GET /resource/edit
-  def edit
-    super
-  end
+  # def edit
+  #   super
+  # end
 
   # PUT /resource
-  def update
-    super
-  end
+  # def update
+  #   super
+  # end
 
   # DELETE /resource
   # def destroy
@@ -40,9 +40,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   protected
+
   def update_resource(resource, params)
     resource.update_without_password(params)
   end
+
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
@@ -53,15 +55,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [
-      :avatar,
-      :name,
-      :gender,
-      :sauna_career,
-      :home_sauna,
-      :prefecture_id,
-      :description,
-      :profile
-      ])
+                                        :avatar,
+                                        :name,
+                                        :gender,
+                                        :sauna_career,
+                                        :home_sauna,
+                                        :prefecture_id,
+                                        :description,
+                                        :profile
+                                      ])
   end
 
   # The path used after sign up.
