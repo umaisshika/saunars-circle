836c6322c7be98e3b78e01b438e0b1d36bcb292a
umaisshika <63295568+umaisshika@users.noreply.github.com>
2020-10-24 21:07:45 +0900

    [add]サ飯投稿機能

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -6,4 +6,22 @@ class FoodsController < ApplicationController
   def new
     @food = Food.new
   end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user_id = current_user.id
+    if @food.save
+      flash[:success] = '投稿しました'
+      redirect_to foods_path
+    else
+      flash.now[:danger] = '投稿に失敗しました'
+      render :new
+    end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :visited_sauna, :prefecture_id, :description, :image)
+  end
 end
