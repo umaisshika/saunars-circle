c131e63f648558d36576bf1a66946dbe457763cf
umaisshika <63295568+umaisshika@users.noreply.github.com>
2020-10-28 18:05:49 +0900

    [add]いいねボタン非同期通信に変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,18 @@
 class LikesController < ApplicationController
+  before_action :set_food
+
   def create
     @like = current_user.likes.create(food_id: params[:food_id])
-    flash[:success] = 'お気に入り登録をしました。'
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @like = Like.find_by(food_id: params[:food_id], user_id: current_user.id)
     @like.destroy
-    flash[:success] = 'お気に入りを解除しました。'
-    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def set_food
+    @food = Food.find(params[:food_id])
   end
 end
