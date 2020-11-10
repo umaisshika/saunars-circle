89a19d7704fcac56d9fd1a4de743f83a37ae5949
umaisshika <63295568+umaisshika@users.noreply.github.com>
2020-11-10 18:57:47 +0900

    [update]投稿一覧デザイン変更

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,7 +1,7 @@
 class FoodsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy, :following_foods]
   before_action :set_food, only: [:show, :update, :edit, :destroy]
-  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy, :following_foods]
 
   def index
     @q = Food.ransack(params[:q])
@@ -57,9 +57,9 @@ class FoodsController < ApplicationController
     @users.each do |user|
       @q_following = Food.ransack(params[:q_following])
       @foods = @q_following.result(distinct: true).where(user_id: user.id)
-                  .page(params[:page])
-                  .per(PER)
-                  .order(created_at: :desc)
+                           .page(params[:page])
+                           .per(PER)
+                           .order(created_at: :desc)
     end
   end
 
