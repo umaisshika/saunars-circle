102d96508365759f2e789f21842462f77f19ae3a
umaisshika <63295568+umaisshika@users.noreply.github.com>
2020-11-18 14:07:19 +0900

    [add]通知機能：モデル単体テスト

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,6 +3,7 @@ class Notification < ApplicationRecord
   belongs_to :food, optional: true
   belongs_to :comment, optional: true
 
-  belongs_to :visitor, class_name: 'User', foreign_key: 'visitor_id', optional: true
-  belongs_to :visited, class_name: 'User', foreign_key: 'visited_id', optional: true
+  belongs_to :visitor, class_name: 'User', foreign_key: 'visitor_id'
+  belongs_to :visited, class_name: 'User', foreign_key: 'visited_id'
+  validates :action, presence: true
 end
--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    context "保存できること" do
+      it "いいねされた時に保存されること" do
+        notification = build(:notification, action: "like")
+        expect(notification).to be_valid
+      end
+
+      it "フォローされた時に保存されること" do
+        notification = build(:notification, action: "follow")
+        expect(notification).to be_valid
+      end
+
+      it "コメントされた時に保存されること" do
+        notification = build(:notification, action: "comment")
+        expect(notification).to be_valid
+      end
+    end
+
+    context "保存できないこと" do
+      it "actionが空の場合は保存されないこと" do
+        notification = build(:notification, action: "")
+        notification.valid?
+        expect(notification).not_to be_valid
+      end
+
+      it "通知の送信者が空の場合は保存されないこと" do
+        notification = build(:notification, visitor: nil)
+        notification.valid?
+        expect(notification).not_to be_valid
+      end
+
+      it "通知の受信者が空の場合は保存されないこと" do
+        notification = build(:notification, visited: nil)
+        notification.valid?
+        expect(notification).not_to be_valid
+      end
+    end
+  end
 end
