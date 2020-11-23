e6f3453916c6a65441ee9be41ea61ce57fd5fdf5
umaisshika <63295568+umaisshika@users.noreply.github.com>
2020-11-23 12:49:37 +0900

    [add]S3設定修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,9 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
   process resize_to_fill: [720, 720, 'Center']
-  # Choose what kind of storage to use for this uploader:
+
   if Rails.env.development? || Rails.env.test?
     storage :file
   else
