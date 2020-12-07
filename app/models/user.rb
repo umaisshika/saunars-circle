5d978d1c121ac0c602e5cbd68de964de0d50a5ba
umaisshika <63295568+umaisshika@users.noreply.github.com>
2020-12-07 23:55:09 +0900

    [fix]rubocopによるコード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,9 +22,9 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_foods, through: :likes, source: :food
-  has_many :relationships, class_name: 'Relationship',foreign_key: 'user_id',dependent: :destroy
+  has_many :relationships, class_name: 'Relationship', foreign_key: 'user_id', dependent: :destroy
   has_many :followings, through: :relationships, source: :follow
-  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id',dependent: :destroy
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id', dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :user
   has_many :active_notifications, class_name: 'Notification', foreign_key: 'visitor_id', dependent: :destroy
   has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visited_id', dependent: :destroy
