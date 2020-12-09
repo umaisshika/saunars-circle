c63ed71847e7f41dbd36d75ac4cc66fb3dfb49c0
umaisshika <63295568+umaisshika@users.noreply.github.com>
2020-12-10 01:43:18 +0900

    [add]ユーザーシステムテスト：テスト項目追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,7 +4,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
   before_action :authenticate_user!
-  before_action :check_guest, only: [:update]
+  before_action :check_guest, only: [:update, :destroy]
   # GET /resource/sign_up
   def new
     super
--- spec/system/users_registration_spec.rb
+++ spec/system/users_registration_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'ユーザー登録', type: :system do
-  let!(:user) { FactoryBot.create(:user, name: '一般ユーザー') }
-  let!(:other_user) { FactoryBot.create(:user, name: 'その他ユーザー') }
+  let!(:user) { create(:user, name: '一般ユーザー') }
+  let!(:other_user) { create(:user, name: 'その他ユーザー') }
+  let!(:admin_user) { create(:user, name: '管理ユーザー', admin: true) }
 
   describe '登録成功' do
     before do
@@ -41,12 +42,40 @@ RSpec.describe 'ユーザー登録', type: :system do
       click_on '更新する'
       expect(page).to have_content 'アカウント情報を変更しました。'
     end
+
+    it 'ゲストユーザーはプロフィールを編集出来ないこと' do
+      visit new_user_session_path
+      click_link 'ゲストユーザーでログイン'
+      visit edit_user_registration_path(user)
+      attach_file 'user[avatar]', 'spec/factories/images/test.jpg'
+      fill_in '自己紹介', with: '毎週水曜は欠かさずホームサウナに通ってます'
+      click_on '更新する'
+      expect(page).to have_content 'ゲストユーザーの変更・削除できません。'
+    end
   end
 
   describe '詳細表示機能' do
     it 'ログイン状態に関わらずユーザーが表示されること' do
       visit user_path(user)
       expect(page).to have_content user.name
+    end
+  end
+
+  describe '削除機能', js: true do
+    it '管理者はユーザーを削除できること' do
+      login(admin_user)
+      visit user_path(user)
+      expect {
+        click_on '削除する'
+        page.accept_confirm "削除しますか？"
+        expect(page).to have_content "「一般ユーザー」は正常に削除されました"
+      }.to change { User.count }.by(-1)
+    end
+
+    it '管理者権限のないユーザーはユーザーを削除リンクが表示されないこと' do
+      login(user)
+      visit user_path(user)
+      expect(page).not_to have_content "削除する"
     end
   end
 end
