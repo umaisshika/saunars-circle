c8e8809bc271b52e8f6af74c077a3cc18dfc2626
umaisshika <63295568+umaisshika@users.noreply.github.com>
2020-12-12 09:44:44 +0900

    [fix]フォロー機能システムテスト：リファクタリング

--- spec/system/relationships_spec.rb
+++ spec/system/relationships_spec.rb
@@ -1,38 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe 'ユーザーフォロー機能', type: :system do
-  let!(:user) { FactoryBot.create(:user, name: '一般ユーザー') }
-  let!(:other_user) { FactoryBot.create(:user, name: 'その他ユーザー') }
+  let!(:user) { create(:user, name: '一般ユーザー') }
+  let!(:other_user) { create(:user, name: 'その他ユーザー') }
 
   describe '他のユーザーをフォローしていない場合' do
     it 'フォローできること', js: true do
       login user
       visit user_path(other_user)
-      expect(other_user.followers.count).to eq(0)
-      click_on 'お気に入り'
-      sleep 1
-      expect(page).to have_button 'お気に入り済み'
-      expect(other_user.followers.count).to eq(1)
+      expect {
+        click_on 'お気に入り'
+        expect(page).to have_button 'お気に入り済み'
+      }.to change { other_user.followers.count }.by(1)
     end
 
     it 'フォロワー一覧を表示してる状態でフォロワーが追加されること', js: true do
       login user
       visit followers_user_path(other_user)
+      expect {
       click_on 'お気に入り'
-      expect(page).to have_button 'お気に入り済み'
-      expect(page).to have_content user.name
+        expect(page).to have_button 'お気に入り済み'
+        expect(page).to have_content user.name
+      }.to change { other_user.followers.count }.by(1)
     end
   end
 
   describe '他のユーザーをフォローしている場合' do
-    let!(:relationship) { FactoryBot.create(:relationship, user_id: user.id, follow_id: other_user.id) }
+    let!(:relationship) { create(:relationship, user_id: user.id, follow_id: other_user.id) }
 
     it 'ユーザーのフォロー状況が表示されていること' do
       visit followings_user_path(user)
       expect(page).to have_content other_user.name
     end
 
-    it 'ユーザーの非フォロー状況が表示されていること' do
+    it 'ユーザーのフォロワーが表示されていること' do
       visit followers_user_path(other_user)
       expect(page).to have_content user.name
     end
@@ -40,18 +41,20 @@ RSpec.describe 'ユーザーフォロー機能', type: :system do
     it 'フォロー解除できること', js: true do
       login user
       visit user_path(other_user)
-      expect(other_user.followers.count).to eq(1)
-      click_on 'お気に入り済み'
-      expect(page).to have_button 'お気に入り'
-      expect(other_user.followers.count).to eq(0)
+      expect {
+        click_on 'お気に入り済み'
+        expect(page).to have_button 'お気に入り'
+      }.to change { other_user.followers.count }.by(-1)
     end
 
     it 'フォロワー一覧を表示してる状態でフォロワーが削除されること', js: true do
       login user
       visit followers_user_path(other_user)
       expect(page).to have_content user.name
-      click_on 'お気に入り済み'
-      expect(page).to_not have_content user.name
+      expect {
+        click_on 'お気に入り済み'
+        expect(page).to_not have_content user.name
+      }.to change { other_user.followers.count }.by(-1)
     end
   end
 end
--- spec/system/users_session_spec.rb
+++ spec/system/users_session_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'セッション管理', type: :system do
-  let!(:user) { FactoryBot.create :user }
+  let!(:user) { create(:user) }
 
   describe 'ログイン成功' do
     it 'ログインできること' do
